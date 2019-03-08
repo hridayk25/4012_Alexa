6a162a38b0278075c7e6daf4adcbbf2ff2d6f922
vtanguturi <vtanguturi3@gatech.edu>
2019-03-08 16:02:42 -0500

    Fixed duplicate naming

--- parse_dict.py
+++ parse_dict.py
@@ -4,14 +4,14 @@ from nltk.tokenize import MWETokenizer
 
 def init_base_order_tokenizer():
     food_tokenizer = MWETokenizer()
-    items = {}
+    food_items = {}
     with open('ChickfilAmenuitems - Sheet1.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             food_item = row['Menu Item'].replace(' ', '_').lower()
-            items[food_item] = 0
+            food_items[food_item] = 0
             items = row['Menu Item'].split(' ')
             if len(items) > 1:
                 item_words = tuple(row['Menu Item'].lower().split(' '))
                 food_tokenizer.add_mwe(item_words)
-    return food_tokenizer, items
+    return food_tokenizer, food_items
